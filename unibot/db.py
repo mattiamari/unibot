20c783d84d5afce7657b666e1c6025e69c24096b
Mattia Mari <mattiamari96@gmail.com>
2019-04-12 20:11:59 +0200

    increase db connection timeout

--- unibot/db.py
+++ unibot/db.py
@@ -10,7 +10,7 @@ conn_string = ('mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4'
 
 engine = create_engine(conn_string,
                        echo=False,
-                       pool_recycle=120,
+                       pool_recycle=3500,  # a couple seconds lower than mysql's wait_timeout
                        pool_size=5,
                        max_overflow=20)
 
